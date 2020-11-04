f14f0612027a7c8956932dcfa3cd876a33e8fa9b
tacslouai <55443256+tacslouai@users.noreply.github.com>
2020-11-04 16:49:25 -0500

    worked on a121

--- python-dev/a121_catch_a_turtle_lh/a121_catch_a_turtle_lh.py
+++ python-dev/a121_catch_a_turtle_lh/a121_catch_a_turtle_lh.py
@@ -1,22 +1,43 @@
 # a121_catch_a_turtle.py
+# Screen Size: 400x300
 #-----import statements-----
-import turtle
+import turtle, random
 
 #-----game configuration----
 turt_color = 'darkgreen'
 turt_size = 5
-turt_shape = 'classic'
+turt_shape = 'circle'
 
+score = 0
 #-----initialize turtle-----
 turt = turtle.Turtle(shape = turt_shape)
 turt.turtlesize(turt_size)
 turt.color(turt_color)
+turt.speed(0)
+turt.penup()
 
+score_writer = turtle.Turtle()
 #-----game functions--------
-def on_click():
-    print('nut')
+def turt_clicked(x, y):
+    change_position()
+    update_score()
+
+def change_position():
+    newx = random.randint(-400, 400)
+    newy = random.randint(-300, 300)
+    turt.goto(newx, newy)
+
+def update_score():
+    global score
+    score += 1
+    print(score)
 
 #-----events----------------
+turt.onclick(turt_clicked)
+
+
+
+
 
 wn = turtle.Screen()
 wn.mainloop()
