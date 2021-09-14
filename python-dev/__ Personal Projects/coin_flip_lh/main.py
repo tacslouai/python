f77e5a0c7f4ed6f6d712925c630fb6e35a255634
tacslouai <55443256+tacslouai@users.noreply.github.com>
2021-09-14 00:15:27 -0400

    Added automatic play

--- python-dev/__ Personal Projects/coin_flip_lh/main.py
+++ python-dev/__ Personal Projects/coin_flip_lh/main.py
@@ -1,62 +1,107 @@
 import time, random
 
-def main():
-    cookies = 0
-    coinpos = ['heads', 'tails']
-    coin = ' '
+question = ""
+cookie_list = []
+positions = []
+counter = 0
 
+def main(human):
+    global cookie_list, positions, counter
+    cookies = 500
+    coinpos = ['heads', 'tails']
+
+    coin = ' '
+    
     while(True):
         u_time = time.time()
         seed = int(u_time * 1000)
         random.seed(seed)
         coin_num = (int(random.random()*10000000000))
-
+        bot_num = random.randint(0,1)
+        bet = 10
+        prebet = bet
         if(coin_num % 2 == 0):
             coin = coinpos[0]
         else:
             coin = coinpos[1]
-        print("A coin is flipped...")
-        print("Guess which side it landed on! (h for Heads/t for Tails)")
-        user_choice = input()
 
-        if(user_choice == coin):
-            print("Congrats! You got it right!")
-            print("Here's a cookie, you deserve it")
-            print("+1 Cookie")
-            cookies += 1
-            
-            print("You currently have", cookies, "cookies")
+        
+        if(human):
+            print("A coin is flipped...")
+            print("Guess which side it landed on! (h for Heads/t for Tails)")
+            user_choice = input().lower().strip()
 
-        elif(user_choice != coin):
-            print("Damn bro... you got it wrong")
-            print("Sorry to do this to you but...")
-            print("-1 Cookie")
-            cookies -= 1
-            print("You currently have", cookies, "cookies")
+            if(user_choice == coin):
+                print("Congrats! You got it right!")
+                print("Here's a cookie, you deserve it")
+                print("+1 Cookie")
+                cookies += 1
+                
+                print("You currently have", cookies, "cookies")
 
-        elif(user_choice == 'q' or user_choice == 'Q'):
-            return 0
-            
-        else:
-            print("Invalid Input!")
+            elif(user_choice != coin):
+                print("Damn bro... you got it wrong")
+                print("Sorry to do this to you but...")
+                print("-1 Cookie")
+                cookies -= 1
+                print("You currently have", cookies, "cookies")
 
-'''
-        invalid = True
-        while(invalid == True):
-            user_choice = input("q to quit, r to repeat...")
-            if(user_choice == 'q'):
-                invalid = False
-                print("Thanks for playing! :)")
+            elif(user_choice == 'q'):
                 return 0
-            elif(user_choice == 'r'):
-                print()
-                invalid = False
+                
             else:
                 print("Invalid Input!")
-                invalid = True
-'''
-    
         
-        
+        elif(not human):
+            #print("Bot play beginning...\n")
+            #time.sleep(0.0001)
+            bot_choice = coinpos[1]
+            if bot_choice == coin:
+                cookies += bet
+                #bet = bet / 2
+                
+            elif bot_choice != coin:
+                cookies -= bet
+                #bet = bet * 2
+            
+            counter += 1
+            positions.append(counter)
+            cookie_list.append(cookies)
+            
+            print("Cookies: ", cookies)
+            print(bet)
+            print("Counter: ", counter)
+            
+            if counter >= 100000:
+                break
+                
+        else:
+            print('idk what happened')
+            break
+            
+            
 
-main()
+while(question != "answered"):    
+    question = input("Are you a human?").strip().lower()
+    if question == "yes" or question == "y":
+        print()
+        question = "answered"
+        human = True
+    elif question == "no" or question == "n":
+        print()
+        question = "answered"
+        human = False
+    else: 
+        question = "unanswered"
+
+main(human)
+
+write_cookies = open( 'coin_flip_lh\cookies.txt', 'w' )
+for a in cookie_list:
+    write_cookies.write(str(a) + '\n' )
+write_cookies.close()
+    
+write_pos = open("coin_flip_lh\position.txt", 'w')
+for a in positions:
+    write_pos.write(str(a) + '\n')
+write_pos.close()
